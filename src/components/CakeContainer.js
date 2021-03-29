16874950cedee5fc5f0739f9e54d3057b5c06b6a
04alexklink <alex_klink123@hotmail.co.uk>
2021-03-29 11:40:21 +0100

    redux and react-redux libraries installed. CakeContainer component created and imported into App component. This component for now just displays h2 tag and button saying Buy Cake

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+
+const CakeContainer = () => {
+    return (
+        <div>
+          <h1>Number of Cakes:</h1>
+          <button>Buy Cake</button>
+        </div>
+    )
+}
+
+export default CakeContainer
+
