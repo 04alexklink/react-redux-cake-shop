35b8ea813ccdacec9db54e24f0073e5ff58da5df
04alexklink <alex_klink123@hotmail.co.uk>
2021-03-30 11:29:22 +0100

    HooksCakeContainer alternative component created. Similar to original cakeContainer component, but using react-redux hooks to access state from redux within the react component, using the useSelector hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import {Provider} from 'react-redux'
 import store from './redux/store'
-import CakeContainer from './components/CakeContainer'
+// import CakeContainer from './components/CakeContainer'
+import HooksCakeContainer from './components/HooksCakeContainer'
 
 function App() {
   return (
     <Provider store={store}>
     <div className="App">
-      <CakeContainer></CakeContainer>
+      {/* <CakeContainer></CakeContainer> */}
+      <HooksCakeContainer></HooksCakeContainer>
     </div>
     </Provider>
   );
