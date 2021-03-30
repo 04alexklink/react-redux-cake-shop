e872909d74a9e0a2bd9b4057762906f270d29830
04alexklink <alex_klink123@hotmail.co.uk>
2021-03-30 11:45:52 +0100

    useDispatch react-redux hook used to get hold of the redux stores dispatch method. Using it within the react component in the click handler of the button to dispatch the action buyCake.

--- src/components/HooksCakeContainer.js
+++ src/components/HooksCakeContainer.js
@@ -1,11 +1,13 @@
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
+import {buyCake} from '../redux/cakes/cakeActions'
 
 const HooksCakeContainer = () => {
+    const dispatch = useDispatch()
     const numOfCakes = useSelector(state => state.numOfCakes)
     return (
         <div>
           <h2>Number of cakes: {numOfCakes}</h2>
-          <button>Buy Cake</button>
+          <button onClick={() => dispatch(buyCake())}>Buy Cake</button>
         </div>
     )
 }
